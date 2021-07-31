8889de392466fd500291a65ecc730c38d9b0e539
Venturh <venturh94@gmail.com>
2021-08-01 00:13:00 +0200

    fix nexus import

--- src/pages/api/graphql.ts
+++ src/pages/api/graphql.ts
@@ -2,6 +2,7 @@ import { ApolloServer } from 'apollo-server-micro';
 import schema from '../../graphql/schema';
 import context from '../../graphql/context';
 
+export type { Context } from '../../graphql/context';
 const server = new ApolloServer({
 	schema,
 	context,
