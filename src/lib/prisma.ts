0354e87ca7793e5571d65ad45abbb58a1fd74730
Venturh <venturh94@gmail.com>
2022-04-08 19:29:07 +0200

    use yarn

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '.prisma/client';
+import { PrismaClient } from '@prisma/client';
 
 // Prevent multiple instances of Prisma Client in development
 declare const global: NodeJS.Global & { prisma?: PrismaClient };
